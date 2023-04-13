419aad6e1bd82641b870885125c12072530d58e2
solarpark7346 <solarpark7346@gmail.com>
2023-04-13 21:15:16 +0900

    add: rank 추가

---
+++ src/main/java/com/develop/web/domain/rank/service/RankService.java
@@ -0,0 +1,12 @@
+package com.develop.web.domain.rank.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RankService {
+    /**
+     * @description 직급 데이터 불러오기 서비스
+     * Integer 상위 직급 ID, String 직급 이름
+     */
+    List<Map<Integer, String>> getList();
+}
