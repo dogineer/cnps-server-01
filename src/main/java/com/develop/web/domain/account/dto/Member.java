fb2032e9bd90de14833d0be7dd55eb5308c6afb0
solarpark7346 <solarpark7346@gmail.com>
2023-04-15 01:36:24 +0900

    update: 멤버 DTO rankId, deptId 네이밍 변경

--- src/main/java/com/develop/web/domain/account/dto/Member.java
+++ src/main/java/com/develop/web/domain/account/dto/Member.java
@@ -8,8 +8,8 @@ import java.util.Date;
 @Data
 public class Member {
     private Integer id;                 // 유저 PK
-    private Integer dept_id;            // 부서 ID
-    private Integer rank_id;            // 직급 ID
+    private Integer deptId;             // 부서 ID
+    private Integer rankId;             // 직급 ID
     private String account;             // 유저 아이디
     private String name;                // 이름
     private Date birth;                 // 생년월일
@@ -20,8 +20,8 @@ public class Member {
     private LocalDateTime joined_at;    // 가입 날짜
     private LocalDateTime approved_at;  // 승인 날짜
 
-    private int access;                 // 가입 승인 요청 플래그
-    private int del_fl;                 // 유저 삭제 플래그
+    private Integer access;             // 가입 승인 요청 플래그
+    private Integer del_fl;             // 유저 삭제 플래그
 
     private String password;            // 패스워드
     private String verifyPassword;      // 패스워드 확인
--- src/main/java/com/develop/web/domain/account/dto/MemberInfo.java
+++ src/main/java/com/develop/web/domain/account/dto/MemberInfo.java
@@ -1,4 +1,4 @@
-package com.develop.web.domain.auth.dto;
+package com.develop.web.domain.account.dto;
 
 import lombok.Data;
 
@@ -9,11 +9,12 @@ import java.util.Date;
 public class MemberInfo {
     private String account;
     private String name;
-    private String rank;
-    private String dept;
-    private Date birth;
+    private String rankId;
+    private String deptId;
+    private LocalDateTime birth;
     private Byte gender;
     private String email;
     private String des;
+    private Byte access;
     private LocalDateTime approved_at;
 }
