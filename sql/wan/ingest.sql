980081eb377f1f446b08dc41537cd7b963356ee0
solarpark7346 <solarpark7346@gmail.com>
2023-04-18 22:29:00 +0900

    add ingest, metadata sql 추가

--- sql/wan/ingest.sql
+++ sql/wan/ingest.sql
@@ -1,6 +1,7 @@
 CREATE TABLE ingest (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'PK 프로그램 코드',
-    f_member_id INT NOT NULL COMMENT '멤버 id' AS,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '게시물 ID',
+    f_member_id INT NOT NULL COMMENT '멤버 ID',
+    f_media_uuid VARCHAR(32) COMMENT '영상 uuid',
     create_at DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT '생성날짜',
     title VARCHAR(255) COMMENT '제목',
     program VARCHAR(32) COMMENT '프로그램명',
