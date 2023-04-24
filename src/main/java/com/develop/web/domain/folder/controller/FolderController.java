d8d4543abbecb4ef7f9773bb293359ba9825e4ee
solarpark7346 <solarpark7346@gmail.com>
2023-04-24 20:40:59 +0900

    update: 스웨거 정리 및 하위 폴더 조회 추가

--- src/main/java/com/develop/web/domain/folder/controller/FolderController.java
+++ src/main/java/com/develop/web/domain/folder/controller/FolderController.java
@@ -1,31 +1,36 @@
 package com.develop.web.domain.folder.controller;
 
 import com.develop.web.domain.folder.dto.FolderDto;
+import com.develop.web.domain.folder.service.ChildrenFolderFetcher;
 import com.develop.web.domain.folder.service.CreateNewFolder;
-import com.develop.web.domain.folder.service.FolderListFetcher;
+import com.develop.web.domain.folder.service.RootFolderListFetcher;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
 @RequiredArgsConstructor
+@RestController
+@Tag(name = "프로그램 관리", description = "Swagger 테스트용 API")
 @RequestMapping(value = "/folder")
 public class FolderController {
     private final CreateNewFolder createNewFolder;
-    private final FolderListFetcher folderListFetcher;
+    private final RootFolderListFetcher rootFolderListFetcher;
+    private final ChildrenFolderFetcher childrenFolderFetcher;
 
     @PostMapping("/create")
+    @Operation(summary = "상위 폴더 조회", description = "상위 폴더에 해당하는 폴더만 조회합니다.")
     public String createNewFolder(String name){
         createNewFolder.addFolder(name);
-        return "pages/home";
+        return "redirect:/home";
     }
 
-    @GetMapping("/show")
-    public void ShowFolderList(){
-        List<FolderDto> folderDatas = folderListFetcher.getFolder();
+    @GetMapping("/show/{num}")
+    @Operation(summary = "하위 폴더 조회", description = "상위 폴더 안에 있는 하위 폴더를 조회합니다.")
+    public List<FolderDto> ShowFolderList(@PathVariable Integer num){
+
+        return childrenFolderFetcher.getChildrenFolder(num);
     }
 }
