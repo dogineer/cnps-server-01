157f3966cf27e32155cd5f3b2a2457c91a8750b4
solarpark7346 <solarpark7346@gmail.com>
2023-04-27 20:33:29 +0900

    fix: 오타 수정

--- src/main/java/com/develop/web/domain/folder/controller/FolderController.java
+++ src/main/java/com/develop/web/domain/folder/controller/FolderController.java
@@ -3,7 +3,6 @@ package com.develop.web.domain.folder.controller;
 import com.develop.web.domain.folder.dto.FolderDto;
 import com.develop.web.domain.folder.service.ChildrenFolderFetcher;
 import com.develop.web.domain.folder.service.CreateNewFolder;
-import com.develop.web.domain.folder.service.RootFolderListFetcher;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +12,10 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@Tag(name = "프로그램 관리", description = "Swagger 테스트용 API")
+@Tag(name = "프로그램 폴더 관리", description = "폴더 생성 및 조회")
 @RequestMapping(value = "/folder")
 public class FolderController {
     private final CreateNewFolder createNewFolder;
-    private final RootFolderListFetcher rootFolderListFetcher;
     private final ChildrenFolderFetcher childrenFolderFetcher;
 
     @PostMapping("/create")
@@ -26,8 +24,14 @@ public class FolderController {
         createNewFolder.addFolder(folderDto);
     }
 
+    @GetMapping("/show/all")
+    @Operation(summary = "폴더 전체 조회", description = "폴더 전체를 조회합니다.")
+    public void ShowAllFolder(@RequestBody FolderDto folderDto){
+        createNewFolder.addFolder(folderDto);
+    }
+
     @GetMapping("/show/{num}")
-    @Operation(summary = "하위 폴더 조회", description = "상위 폴더 안에 있는 하위 폴더를 조회합니다.")
+    @Operation(summary = "하위 폴더 조회", description = "상위 폴더 안에 있는 하위 폴더를 조회합니다. 상위 폴더의 코드를 입력해주세요.")
     public List<FolderDto> ShowFolderList(@PathVariable Integer num){
 
         return childrenFolderFetcher.getChildrenFolder(num);
