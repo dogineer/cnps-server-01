2ccc5af524fa5de9760f03b3338654fcfce7264d
solarpark7346 <solarpark7346@gmail.com>
2023-05-18 15:32:27 +0900

    update: 공지사항 컨트롤러 스웨거 정보 추가

--- src/main/java/com/develop/web/domain/notice/controller/NoticeController.java
+++ src/main/java/com/develop/web/domain/notice/controller/NoticeController.java
@@ -2,26 +2,27 @@ package com.develop.web.domain.notice.controller;
 
 import com.develop.web.domain.notice.service.PostListFetcher;
 import com.develop.web.domain.notice.dto.PostDto;
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "공지사항", description = "")
 @RequestMapping(value = "/notice")
 public class NoticeController {
 
     private final PostListFetcher postListFetcher;
 
-    public NoticeController(PostListFetcher postListFetcher) {
-        this.postListFetcher = postListFetcher;
-    }
-
-    /*
-    * 공지사항 글 작성
-    * */
     @PostMapping(value = "/create")
+    @Operation(summary = "공지사항 작성", description = "공지사항을 작성합니다.")
     public String createNotice(PostDto writer, HttpSession session){
         Integer empId = session.getAttribute("empId").hashCode();
         writer.setEmpId(empId);
@@ -30,13 +31,9 @@ public class NoticeController {
         return "redirect:/management/employee";
     }
 
-    /*
-    * 공지사항 리스트 받아오기
-    * */
-    @PostMapping(value = "/list")
-    public String Post(){
-
-        postListFetcher.getPost();
-        return "redirect:/Administrator/employee";
+    @GetMapping(value = "/list")
+    @Operation(summary = "공지사항 목록", description = "공지사항 데이터를 받아옵니다.")
+    public List<PostDto> Post(){
+        return postListFetcher.getPost();
     }
 }
