9ac0e732cc021dd8bf58a0d9d33dc7a0c855772f
solarpark7346 <solarpark7346@gmail.com>
2023-05-18 15:37:30 +0900

    add: 폴더그룹 DTO 추가

---
+++ src/main/java/com/develop/web/domain/media/groupclip/dto/GFolderDto.java
@@ -0,0 +1,11 @@
+package com.develop.web.domain.media.groupclip.dto;
+
+import lombok.Data;
+
+@Data
+public class GFolderDto {
+    private Integer teamId;
+    private String teamName;
+    private Integer folderId;
+    private String folderName;
+}
