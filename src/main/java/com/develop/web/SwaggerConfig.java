f3f95b39cc513553cbb5f120523d7cf0f4810196
solarpark7346 <solarpark7346@gmail.com>
2023-05-20 09:07:53 +0900

    add: swagger config

---
+++ src/main/java/com/develop/web/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.develop.web;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+     @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("NPS 데모 프로젝트")
+                .version("ALPHA 20230519")
+                .description("Network production system API Document");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
