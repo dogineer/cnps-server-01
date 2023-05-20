97234dd44a0ed3253e4eba56f56d60724da2c9e8
solarpark7346 <solarpark7346@gmail.com>
2023-05-20 09:15:33 +0900

    add: config 폴더 변경

--- src/main/java/com/develop/web/global/config/SwaggerConfig.java
+++ src/main/java/com/develop/web/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.develop.web;
+package com.develop.web.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
--- src/main/java/com/develop/web/global/config/WebConfig.java
+++ src/main/java/com/develop/web/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.develop.web;
+package com.develop.web.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
