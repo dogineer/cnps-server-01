b1567eea24fab136f3722dccab569df831c4c577
solarpark7346 <solarpark7346@gmail.com>
2023-05-24 20:18:01 +0900

    add: 널 에러 추가

--- src/main/java/com/develop/web/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/develop/web/global/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.develop.web.global.exception.code.CommonErrorCode;
 import com.develop.web.global.exception.code.ErrorCode;
 import com.develop.web.global.exception.response.ErrorResponse;
 import com.develop.web.global.exception.exception.RestApiException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,13 +13,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    /* 런타임 에러 처리 */
     @ExceptionHandler(RestApiException.class)
     public ResponseEntity<Object> handleCustomException(RestApiException e) {
         return handleExceptionInternal(e.getErrorCode());
     }
 
+    /* 널 에러 처리 */
+    @ExceptionHandler(NullPointerException.class)
+    protected ResponseEntity<Object> handleNullPointerException(NullPointerException e) {
+        log.error("handleNullPointerException", e);
+        return handleExceptionInternal(CommonErrorCode.RESOURCE_NOT_FOUND);
+    }
+
+    /* 모든 에러 처리 */
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handleAllException(Exception e) {
         log.warn("handleAllException", e);
