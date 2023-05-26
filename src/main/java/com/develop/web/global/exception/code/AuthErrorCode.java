90dbe3e072168259d99b19cc30edbc425b99e428
solarpark7346 <solarpark7346@gmail.com>
2023-05-26 20:11:27 +0900

    update: auth 글로벌 예외처리

--- src/main/java/com/develop/web/global/exception/code/AuthErrorCode.java
+++ src/main/java/com/develop/web/global/exception/code/AuthErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
 
-    ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "account 세션 null"),
+    ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "로그인 후 사용해주세요."),
     AUTH_ACCESS_NOT_FOUND(HttpStatus.UNAUTHORIZED, "승인된 계정이 아닙니다.")
     ;
 
--- src/main/java/com/develop/web/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/develop/web/global/exception/handler/GlobalExceptionHandler.java
@@ -1,50 +1,39 @@
 package com.develop.web.global.exception.handler;
 
-import com.develop.web.global.exception.code.CommonErrorCode;
-import com.develop.web.global.exception.code.ErrorCode;
-import com.develop.web.global.exception.response.ErrorResponse;
-import com.develop.web.global.exception.exception.RestApiException;
+import com.develop.web.global.exception.exception.AuthApiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice
+import javax.servlet.http.HttpSession;
+
+
 @Slf4j
+@Controller
+@ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    /* 런타임 에러 처리 */
-    @ExceptionHandler(RestApiException.class)
-    public ResponseEntity<Object> handleCustomException(RestApiException e) {
-        return handleExceptionInternal(e.getErrorCode());
+    @ExceptionHandler(Exception.class)
+    public String handleAllException(Exception e, Model model) {
+        model.addAttribute("error", e.toString());
+        model.addAttribute("message", e.getMessage());
+
+        return "error/view";
     }
 
-    /* 널 에러 처리 */
-    @ExceptionHandler(NullPointerException.class)
-    protected ResponseEntity<Object> handleNullPointerException(NullPointerException e) {
-        log.error("handleNullPointerException", e);
-        return handleExceptionInternal(CommonErrorCode.RESOURCE_NOT_FOUND);
-    }
+    @ExceptionHandler(AuthApiException.class)
+    public String sessionNullException(AuthApiException e, HttpSession session, Model model) {
+        log.error(e.getErrorCode().toString());
 
-    /* 모든 에러 처리 */
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<Object> handleAllException(Exception e) {
-        log.warn("handleAllException", e);
-        return handleExceptionInternal(CommonErrorCode.INTERNAL_SERVER_ERROR);
-    }
+        model.addAttribute("error", e.getErrorCode());
+        model.addAttribute("message", e.getErrorCode().getMessage());
 
-    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
-        return ResponseEntity.status(errorCode.getHttpStatus())
-            .body(buildErrorResponse(errorCode));
-    }
-
-    private ErrorResponse buildErrorResponse(ErrorCode errorCode) {
-        return ErrorResponse.builder()
-            .code(errorCode.name())
-            .message(errorCode.getMessage())
-            .build();
+        session.invalidate();
+        return "error/view";
     }
 }
