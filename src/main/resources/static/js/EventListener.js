17e53c12fb03666f964a6f0dea36c692efa42f48
solarpark7346 <solarpark7346@gmail.com>
2023-07-06 21:49:24 +0900

    add: 검색 추가 및 이벤트 리스너 교체 작업중

---
+++ src/main/resources/static/js/EventListener.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var searchButton = document.getElementById('search-button');
+    searchButton.addEventListener('click', handleSearch);
+});
