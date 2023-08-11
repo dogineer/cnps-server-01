e9cdff66407c963879f473b63ad91695826b9575
solarpark7346 <solarpark7346@gmail.com>
2023-08-11 20:23:17 +0900

    update: GFolderDto 폴더 변경 및 GroupClipsMapper 삭제

--- src/main/java/com/develop/web/domain/media/groupclip/mapper/GroupClipsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.develop.web.domain.media.groupclip.mapper;
-
-import com.develop.web.domain.media.ingest.dto.IngestRequestData;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface GroupClipsMapper {
-    void selectFolderIncludeClips(IngestRequestData ingestRequestData);
-}
