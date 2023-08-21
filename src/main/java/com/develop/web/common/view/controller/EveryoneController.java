335f5f5836c58aa890e05a27ebee27bd8779386c
solarpark7346 <solarpark7346@gmail.com>
2023-08-21 18:30:13 +0900

    update: 권한 페이징 코드 삭제

--- src/main/java/com/develop/web/common/view/controller/EveryoneController.java
+++ src/main/java/com/develop/web/common/view/controller/EveryoneController.java
@@ -19,25 +19,13 @@ public class EveryoneController {
      * @description 권한 없는 모든 유저가 접근 가능한 페이지
      */
     @GetMapping("/")
-    public String index(Model model, HttpSession session) {
+    public String index(Model model) {
         String version = "ALPHA VERSION";
 
         model.addAttribute("Ranks", rankListFetcher.getList());
         model.addAttribute("Depts", findDeptList.getDeptList());
         model.addAttribute("version", version);
 
-        boolean isLogin = session.getAttribute("account") != null;
-
-        if (isLogin) {
-            boolean isAdmin = session.getAttribute("rank") != null && (int) session.getAttribute("rank") == 12;
-
-            if (isAdmin) {
-                return "redirect:/admin/management/user";
-            } else {
-                return "redirect:/user/clip";
-            }
-        } else {
-            return "index";
-        }
+        return "index";
     }
 }
