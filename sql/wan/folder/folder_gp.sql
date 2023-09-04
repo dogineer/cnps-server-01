9a18c254a7ee211d009f5012d5ea14789d127c57
dogineer <dogineer.dev@gmail.com>
2023-09-04 21:20:20 +0900

    sql: convert_metadata 추가

--- sql/wan/folder/folder_gp.sql
+++ sql/wan/folder/folder_gp.sql
@@ -14,11 +14,11 @@ select
     t.name as `team_name`,
     c.folder_id,
     f.name as `folder_name`,
-    c.a_metadata_id,
+    c.archive_metadata_id,
     a.* from clip c
 left join ingest i on i.id = c.ingest_id
 left join team t on t.id = c.team_id
 left join folder f on f.id = c.folder_id
-left join a_metadata a on a.id = c.a_metadata_id
+left join archive_metadata a on a.id = c.archive_metadata_id
 
 where c.folder_id = 4
--- sql/wan/media/a_metadata.sql
+++ sql/wan/media/a_metadata.sql
@@ -1,4 +1,4 @@
-CREATE TABLE a_metadata (
+CREATE TABLE archive_metadata (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '메타 ID',
     clip_uuid VARCHAR(36) NOT NULL UNIQUE KEY COMMENT '영상 uuid',
     create_ymd DATE NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT '생성날짜',
@@ -13,3 +13,19 @@ CREATE TABLE a_metadata (
     duration INT(255) COMMENT '길이',
     size BIGINT COMMENT '용량'
 ) COMMENT '아카이브 메타데이터';
+
+CREATE TABLE convert_metadata (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '메타 ID',
+    clip_uuid VARCHAR(36) NOT NULL UNIQUE KEY COMMENT '영상 uuid',
+    create_ymd DATE NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT '생성날짜',
+    file_path TEXT COMMENT '파일 경로',
+    file_name VARCHAR(255) COMMENT '파일 이름',
+    ext VARCHAR(4) COMMENT '확장자',
+    width INT(32) COMMENT '가로',
+    height INT(32) COMMENT '세로',
+    format_name VARCHAR(32) COMMENT '포맷 이름',
+    format_long_name VARCHAR(32) COMMENT '포맷 전체 이름',
+    tags TEXT COMMENT '영상 태그 정보',
+    duration INT(255) COMMENT '길이',
+    size BIGINT COMMENT '용량'
+) COMMENT '변환 메타데이터';
