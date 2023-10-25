a0035f98056d1ed3fc7d6ee499157eacf7bdd347
dogineer <dogineer.dev@gmail.com>
2023-10-25 09:13:24 +0900

    update: 서비스 코드 ProgramService로 통합 정리

--- src/main/java/com/develop/web/domain/admin/program/controller/AdminProgramController.java
+++ src/main/java/com/develop/web/domain/admin/program/controller/AdminProgramController.java
@@ -16,47 +16,42 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping(value = "/s1/api/admin/program")
 public class AdminProgramController {
-    private final ProgramCreationService programCreationService;
-    private final ProgramDeleteionService programDeleteionService;
-    private final ProgramRecursionListFetcherService programRecursionListFetcherService;
-    private final ProgramTypeFetcherService programTypeFetcherService;
-    private final ProgramPathFetcherService programPathFetcherService;
-    private final ProgramFetcherService programFetcherService;
+    private final ProgramService programService;
 
     @PostMapping("/add")
     @Operation(summary = "프로그램, 유형, 팀 추가하기",
         description = "부모ID가 1이면 유형, 부모ID가 유형ID일 경우 프로그램")
     public void programAdd(@RequestBody ProgramDto programDto) {
-        programCreationService.addProgram(programDto);
+        programService.addProgram(programDto);
     }
 
     @DeleteMapping("/delete/{programId}")
     @Operation(summary = "프로그램 삭제하기", description = "프로그램을 삭제합니다.")
     public void deleteProgram(@PathVariable("programId") Integer programId) {
-        programDeleteionService.removeProgram(programId);
+        programService.removeProgram(programId);
     }
 
     @GetMapping("/list/all")
     @Operation(summary = "전체 팀 리스트", description = "전체 팀 목록을 조회합니다.")
     public List<ProgramRecursionDto> programList() {
-        return programRecursionListFetcherService.findProgramList();
+        return programService.findProgramList();
     }
 
     @GetMapping("/type/list")
     @Operation(summary = "프로그램 유형 리스트", description = "프로그램 유형을 조회합니다.")
     public List<ProgramDto> programTypeList(){
-        return programTypeFetcherService.findProgramTypeList();
+        return programService.findProgramTypeList();
     }
 
     @GetMapping("/find/program/{programId}")
     @Operation(summary = "프로그램 찾기", description = "프로그램을 찾습니다.")
     public List<ProgramDto> programDetails(@PathVariable Integer programId){
-        return programFetcherService.findProgram(programId);
+        return programService.findProgram(programId);
     }
 
     @GetMapping("/find/tree/{programId}")
     @Operation(summary = "프로그램 계층 리스트", description = "프로그램 계층을 조회합니다.")
     public List<ProgramPathDto> programTreeDetails(@PathVariable Integer programId){
-        return programPathFetcherService.findProgramPathList(programId);
+        return programService.findProgramPathList(programId);
     }
 }
---
+++ src/main/java/com/develop/web/domain/admin/program/service/ProgramService.java
@@ -0,0 +1,40 @@
+package com.develop.web.domain.admin.program.service;
+
+import com.develop.web.domain.admin.program.dto.ProgramPathDto;
+import com.develop.web.domain.admin.program.dto.ProgramRecursionDto;
+import com.develop.web.domain.admin.program.mapper.AdminProgramMapper;
+import com.develop.web.domain.users.program.dto.ProgramDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProgramService {
+    private final AdminProgramMapper adminProgramMapper;
+
+    public void addProgram(ProgramDto programDto) {
+        adminProgramMapper.insertProgram(programDto);
+    }
+
+    public void removeProgram(Integer programId) {
+        adminProgramMapper.deleteProgram(programId);
+    }
+
+    public List<ProgramRecursionDto> findProgramList() {
+        return adminProgramMapper.selectProgramRecursionAllList();
+    }
+
+    public List<ProgramDto> findProgramTypeList() {
+        return adminProgramMapper.selectProgramTypeList();
+    }
+
+    public List<ProgramDto> findProgram(Integer programId){
+        return adminProgramMapper.selectProgramList(programId);
+    }
+
+    public List<ProgramPathDto> findProgramPathList(Integer programId){
+        return adminProgramMapper.selectProgramPathList(programId);
+    }
+}
